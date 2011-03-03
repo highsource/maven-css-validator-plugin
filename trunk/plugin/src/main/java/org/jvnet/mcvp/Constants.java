7628e8c0e4dd111bf58383d1d73ee7c2d13b8e94
paul_reeves <paul_reeves@1186c412-0c2b-c4b9-ef53-8c1f125cd759>
2011-03-03 16:38:41 +0000

    created constants file shared between reporting mojo and validation mojo

---
+++ trunk/plugin/src/main/java/org/jvnet/mcvp/Constants.java
@@ -0,0 +1,9 @@
+package org.jvnet.mcvp;
+
+public class Constants {
+
+	private Constants() {
+	}
+
+	public static final String REPORT_LOCATION = "${project.build.directory}/mvcp-report/report.html";
+}
